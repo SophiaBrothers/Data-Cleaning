656d87957519cb25ad96eff06edccee348e2684c
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-09-30 12:57:46 -0500

    Update DATA CLEANING METHODS.sql

--- DATA CLEANING METHODS.sql
+++ DATA CLEANING METHODS.sql
@@ -1,55 +1,25 @@
 ﻿/***********************   DATA CLEANING  *************************************/
-USE BikesDB
-
-Select * from Employees
+USE CleaningDB
 
 
---Problem columns are:  NAME_FIRST,   NAME_LAST,   PHONENUMBER,   VALIDITY_STARTDATE,   VALIDITY_ENDATE
-
-------------------------------------------------------------------------------------------------
-
-
---CLEANING NAME_FIRST & NAME_LAST 
-
---A diamond (or square) with a question mark in the middle. This is a "replacement character."
---It is displayed whenever a character is not recognized in a document or webpage                        
-
-
-
---replace special character
-
---REPLACE(string, old_string, new_string)
-
-select NAME_FIRST, REPLACE(NAME_FIRST COLLATE Latin1_General_BIN, N'�','e') as CLEAN_NAME from Employees;
-select NAME_LAST, REPLACE(NAME_LAST COLLATE Latin1_General_BIN, N'�','e') as CLEAN_NAME from Employees;
-
---COLLATE is used to cast the string or column collation into a specified collation; In this case, Latin1_General_BIN 
---The N denotes that the subsequent string is in Unicode
-
-
---to update in table
-begin transaction
-update Employees 
-set NAME_FIRST = REPLACE(NAME_FIRST COLLATE Latin1_General_BIN,N'�','e')
-
-update Employees 
-set NAME_LAST = REPLACE(NAME_LAST COLLATE Latin1_General_BIN,N'�','e')
-
-
-rollback;
-
-
-
-
-
-
-----------------------------------------------------------------------------------------------
-
-
-SELECT  NAME_FIRST as FirstName,
-        len(NAME_FIRST) as field_length
+SELECT EMPLOYEE_ID as EmployeeID,
+        len(EMPLOYEE_ID) as field_length
 FROM Employees;
 
+
+GO
+
+
+-------------------REMOVING ZERO'S FROM EMPLOYEE_ID------------------------------------------
+
+--basic understanding
+SELECT SUBSTRING('0000000011', 1, 8) AS ExtractString;    -- Extract 8 characters from a string, starting in position 1:
+-- returns 00000000
+
+
+
+
+--------------------------------------------------------------------------------------------------------
 
 --REMOVE UNNECESSARY SPACES
 
@@ -65,7 +35,7 @@ FROM  Employees
 GROUP BY SEX;
 
 
--- to get rid of the unwanted characters (extra spaces) and eliminate the capitalization mistakes.
+-- to get rid of the unwanted characters (extra spaces) and eliminate the capitalization mistakes(LOWER).
 SELECT lower(trim(NAME_FIRST)), count(*)
 FROM  Employees
 GROUP BY lower(trim(NAME_FIRST))
@@ -170,4 +140,4 @@ ROLLBACK
 go
 
 ---------------------------------------------------------------------------------
-select 
+
